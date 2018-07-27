0ceeb80090daf71be5cced6d6e969b0a08bf8b93
reddowell <vikt.anmail@gmail.com>
2018-07-28 01:54:10 +0300

    Progression game

---
+++ src/Games/Progression.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace BrainGames\Games\Progression;
+
+use function  BrainGames\GameStructure\init;
+
+const PROGRESSION_LENGTH = 10;
+const STEP_MIN = 1;
+const STEP_MAX = 10;
+const START_MIN = 0;
+const START_MAX = 100;
+const GAME_DESCRIPTION = 'What number is missing in this progression?';
+
+function run()
+{
+    $questionFunction = function () {
+
+        $step = rand(STEP_MIN, STEP_MAX);
+        $startNumber = rand(START_MIN, START_MAX);
+        $progression = makeProgression($startNumber, $step);
+        $indexToHide = array_rand($progression);
+        $rightAnswer = $progression[$indexToHide];
+        $progression[$indexToHide] = '..';
+        $question = implode(' ', $progression);
+
+        return [$question, $rightAnswer];
+    };
+    init(GAME_DESCRIPTION, $questionFunction);
+}
+
+function makeProgression(int $startNumber, int $step): array
+{
+    $progression = [$startNumber];
+    for ($i = 1; $i < 10; $i++) {
+        $progression[$i] = $progression[$i - 1] + $step;
+    }
+
+    return $progression;
+}
